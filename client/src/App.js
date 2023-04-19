03a113634f6ef2145286d712c0ebea405cfaee8d
emremataraci <mtrcemre@gmail.com>
2023-04-19 17:17:43 +0300

    test pages created

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,13 @@ import FormsPage from "./components/ITPForms/FormsPage";
 import FormEdit from "./components/ITPForms/FormEdit";
 import UploadForm from './components/FormsPage/UploadForm';
 import Gallery from "./components/Gallery/gallery";
+import TestPage1 from "./components/testpages/testpage1";
+import TestPage2 from "./components/testpages/testpage2";
+import TestPage3 from "./components/testpages/testpage3";
+import TestPage4 from "./components/testpages/testpage4";
+import TestPage5 from "./components/testpages/testpage5";
+import TestPage6 from "./components/testpages/testpage6";
+
 
 function App() {
   return (
@@ -30,6 +37,13 @@ function App() {
           <Route path="/forms" element={<FormsPage />} />
           <Route path="/forms/:id" element={<FormEdit />} />
           <Route path='/gallery' element={<Gallery />} />
+          <Route path='/testpage1' element={<TestPage1 />} />
+          <Route path='/testpage2' element={<TestPage2 />} />
+          <Route path='/testpage3' element={<TestPage3 />} />
+          <Route path='/testpage4' element={<TestPage4 />} />
+          <Route path='/testpage5' element={<TestPage5 />} />
+          <Route path='/testpage6' element={<TestPage6 />} />
+
         </Routes>
       </div>
     </BrowserRouter>
