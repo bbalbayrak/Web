00ce3cf5e9100b19d8e02f582b6e19a328d7e39e
emremataraci <mtrcemre@gmail.com>
2023-05-10 11:37:04 +0300

    Create user input fields added

--- client/src/components/Users/CreateUser.js
+++ client/src/components/Users/CreateUser.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { createUser } from './Userapi';
 import { useNavigate } from 'react-router-dom';
 import './CreateUser.css';
+
 const CreateUser = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState('');
@@ -14,7 +15,7 @@ const CreateUser = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     try {
       const userData = {
         email,
@@ -31,27 +32,43 @@ const CreateUser = () => {
       // console.error('Kullanıcı oluşturulamadı:', error);
     }
   };
-  
+
   return (
     <div className="create-user-container">
-    <form className="create-user-form" onSubmit={handleSubmit}>
-      <h2>Kullanıcı Oluştur</h2>
-      <div className="form-group">
-        <label htmlFor="name">İsim:</label>
-        <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
-      </div>
-      <div className="form-group">
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      </div>
-      <div className="form-group">
-        <label htmlFor="phone">Telefon:</label>
-        <input type="tel" id="phone" value={phone} onChange={(e) => setPhone(e.target.value)} />
-      </div>
-      <button type="submit">Kullanıcı Oluştur</button>
-    </form>
-  </div>
-);
+      <form className="create-user-form" onSubmit={handleSubmit}>
+        <h2>Kullanıcı Oluştur</h2>
+        <div className="form-group">
+          <label htmlFor="name">İsim:</label>
+          <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Şifre:</label>
+          <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="phone">Telefon:</label>
+          <input type="tel" id="phone" value={phone} onChange={(e) => setPhone(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="role">Rol:</label>
+          <input type="text" id="role" value={role} onChange={(e) => setRole(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="username">Kullanıcı Adı:</label>
+          <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="relatedCompany">İlgili Şirket:</label>
+          <input type="text" id="relatedCompany" value={relatedCompany} onChange={(e) => setRelatedCompany(e.target.value)} />
+        </div>
+        <button type="submit">Kullanıcı Oluştur</button>
+      </form>
+    </div>
+  );
 };
 
 export default CreateUser;
