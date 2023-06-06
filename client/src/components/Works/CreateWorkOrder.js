6fe817521786192f60d09e57e9afe5c963f4b15f
emremataraci <mtrcemre@gmail.com>
2023-06-06 12:08:14 +0300

    multiple product future added to createnewwork

--- client/src/components/Works/CreateWorkOrder.js
+++ client/src/components/Works/CreateWorkOrder.js
@@ -1,21 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { createWork, createWorkStep, getVendors, getCustomers, getUsers, getProducts, createWorkProduct } from './worksapi';
-import "./CreateWorkOrder.css";
+import {
+  createWork,
+  createWorkStep,
+  getVendors,
+  getCustomers,
+  getUsers,
+  getProducts,
+  createWorkProduct,
+} from './worksapi';
+import './CreateWorkOrder.css';
+import Select from 'react-select';
 
 const CreateWorkOrder = () => {
   const navigate = useNavigate();
   const [workData, setWorkData] = useState({
-    order_number: "",
-    project_number: "",
-    vendor_id: "",
-    customer_id: "",
-    quality_responsible_id: "",
-    inspector_id: "",
-    foreman_id: "",
-    work_type: "Order",
-    state: "",
-    status: "",
+    order_number: '',
+    project_number: '',
+    vendor_id: '',
+    customer_id: '',
+    quality_responsible_id: '',
+    inspector_id: '',
+    foreman_id: '',
+    work_type: 'Order',
+    state: '',
+    status: '',
     creator_id: 1,
     creation_date: new Date().toISOString(),
   });
@@ -55,9 +64,13 @@ const CreateWorkOrder = () => {
   const fetchUsers = async () => {
     try {
       const response = await getUsers(); // Adjust this with your API call
-      const qualityRes = response.data.filter((user) => user.role === 'Quality Responsible');
-      const inspectorRes = response.data.filter((user) => user.role === 'Inspector');
-      const foremanRes = response.data.filter((user) => user.role === 'Foreman');
+      const qualityRes = response.data.filter(
+        user => user.role === 'Quality Responsible'
+      );
+      const inspectorRes = response.data.filter(
+        user => user.role === 'Inspector'
+      );
+      const foremanRes = response.data.filter(user => user.role === 'Foreman');
       setQualityResponsibles(qualityRes);
       setInspectors(inspectorRes);
       setForemen(foremanRes);
@@ -75,30 +88,40 @@ const CreateWorkOrder = () => {
     }
   };
 
-  const handleProductChange = (e) => {
-    const selectedOptions = Array.from(e.target.selectedOptions);
-    setSelectedProducts(selectedOptions.map((option) => Number(option.value)));
+  const handleProductChange = selectedOptions => {
+    if (selectedOptions) {
+      const selectedProducts = selectedOptions.map(option => {
+        const productId = Number(option.value); // Geçerli bir sayıya dönüştürme
+        return productId;
+      });
+      setSelectedProducts(selectedProducts);
+    } else {
+      setSelectedProducts([]);
+    }
   };
 
-  const handleChange = (e) => {
+  const handleChange = e => {
     setWorkData({ ...workData, [e.target.name]: e.target.value });
   };
 
-  const handleSave = async (e) => {
+  const handleSave = async e => {
     e.preventDefault();
     try {
       const createdWork = await createWork(workData);
       const workStepData = {
         work_id: createdWork.work.id,
-        step_name: "New Work",
+        step_name: 'New Work',
         timestamp: new Date().toISOString(),
-        state: "New Work",
-        status: "Open",
+        state: 'New Work',
+        status: 'Open',
       };
       const createdWorkStep = await createWorkStep(workStepData);
-      
+
       for (const productId of selectedProducts) {
-        await createWorkProduct({ work_id: createdWork.work.id, product_id: productId });
+        await createWorkProduct({
+          work_id: createdWork.work.id,
+          product_id: productId,
+        });
       }
       navigate(`/workorders`);
     } catch (error) {
@@ -144,7 +167,7 @@ const CreateWorkOrder = () => {
             onChange={handleChange}
           >
             <option value="">Select Vendor</option>
-            {vendors.map((vendor) => (
+            {vendors.map(vendor => (
               <option key={vendor.id} value={vendor.id}>
                 {vendor.name}
               </option>
@@ -161,7 +184,7 @@ const CreateWorkOrder = () => {
             onChange={handleChange}
           >
             <option value="">Select Customer</option>
-            {customers.map((customer) => (
+            {customers.map(customer => (
               <option key={customer.id} value={customer.id}>
                 {customer.name}
               </option>
@@ -178,7 +201,7 @@ const CreateWorkOrder = () => {
             onChange={handleChange}
           >
             <option value="">Select Quality Responsible</option>
-            {qualityResponsibles.map((qr) => (
+            {qualityResponsibles.map(qr => (
               <option key={qr.id} value={qr.id}>
                 {qr.name}
               </option>
@@ -195,7 +218,7 @@ const CreateWorkOrder = () => {
             onChange={handleChange}
           >
             <option value="">Select Inspector</option>
-            {inspectors.map((inspector) => (
+            {inspectors.map(inspector => (
               <option key={inspector.id} value={inspector.id}>
                 {inspector.name}
               </option>
@@ -204,67 +227,67 @@ const CreateWorkOrder = () => {
         </div>
         <div className="cwo-form-group">
           <label htmlFor="foreman_id">Foreman</label>
-<select
-         name="foreman_id"
-         id="foreman_id"
-         className="cwo-form-control"
-         value={workData.foreman_id}
-         onChange={handleChange}
-       >
-<option value="">Select Foreman</option>
-{foremen.map((foreman) => (
-<option key={foreman.id} value={foreman.id}>
-{foreman.name}
-</option>
-))}
-</select>
-</div>
-<div className="cwo-form-group">
-<label htmlFor="product_id">Product</label>
-<select
-    name="product_id"
-    id="product_id"
-    className="cwo-form-control"
-    onChange={handleProductChange}
->
-    <option value="">Select Product</option>
-    {products.map((product) => (
-        <option key={product.id} value={product.id}>
-            {product.name}
-        </option>
-    ))}
-</select>
-</div>
-<div className="cwo-form-group">
-      <label htmlFor="state">State</label>
-      <input
-        type="text" 
-        name="state"
-        id="state"
-        className="cwo-form-control"
-        value={workData.state}
-        onChange={handleChange}
-        required
-      />
+          <select
+            name="foreman_id"
+            id="foreman_id"
+            className="cwo-form-control"
+            value={workData.foreman_id}
+            onChange={handleChange}
+          >
+            <option value="">Select Foreman</option>
+            {foremen.map(foreman => (
+              <option key={foreman.id} value={foreman.id}>
+                {foreman.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="cwo-form-group">
+          <label htmlFor="product_id">Product</label>
+
+          <Select
+            name="product_id"
+            id="product_id"
+            className="cwo-form-control"
+            onChange={handleProductChange}
+            options={products.map(product => ({
+              value: product.id,
+              label: product.name,
+            }))}
+            isMulti
+            isSearchable
+          />
+        </div>
+        <div className="cwo-form-group">
+          <label htmlFor="state">State</label>
+          <input
+            type="text"
+            name="state"
+            id="state"
+            className="cwo-form-control"
+            value={workData.state}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="cwo-form-group">
+          <label htmlFor="status">Status</label>
+          <input
+            type="text"
+            name="status"
+            id="status"
+            className="cwo-form-control"
+            value={workData.status}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit" className="cwo-btn cwo-btn-primary">
+          Save Work Order
+        </button>
+      </form>
     </div>
-    <div className="cwo-form-group">
-      <label htmlFor="status">Status</label>
-      <input
-        type="text"
-        name="status"
-        id="status"
-        className="cwo-form-control"
-        value={workData.status}
-        onChange={handleChange}
-        required
-      />
-    </div>
-<button type="submit" className="cwo-btn cwo-btn-primary">
-Save Work Order
-</button>
-</form>
-</div>
-);
+  );
 };
 
 export default CreateWorkOrder;
