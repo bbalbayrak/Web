884f63051c501357c2ece915e4395570004aa22d
yenademir <yena@yenaengineering.nl>
2023-06-30 01:34:02 +0300

    OdooToWeb bug fixing

--- server/models/odootoweb.js
+++ server/models/odootoweb.js
@@ -5,7 +5,7 @@ const OdooToWebWork = {
 
   create: async (order_number, project_number, vendor_id, customer_id, quality_responsible_id, inspector_id, foreman_id, work_type, state, status, creator_name, creation_date, order_id) => {
     const result = await db.one(
-      `INSERT INTO ${Work.tableName} (order_number, project_number, vendor_id, customer_id, quality_responsible_id, inspector_id, foreman_id, work_type, state, status, creator_name, creation_date, order_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *`,
+      `INSERT INTO ${OdooToWebWork.tableName} (order_number, project_number, vendor_id, customer_id, quality_responsible_id, inspector_id, foreman_id, work_type, state, status, creator_name, creation_date, order_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *`,
       [order_number, project_number, vendor_id, customer_id, quality_responsible_id, inspector_id, foreman_id, work_type, state, status, creator_name, creation_date, order_id]
     );
     return result;
