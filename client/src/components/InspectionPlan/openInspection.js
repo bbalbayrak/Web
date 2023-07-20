b3ee371ad01888f647842adcd3a5c24ab160c155
yenademir <yena@yenaengineering.nl>
2023-07-20 16:25:11 +0300

    Control Method

--- client/src/components/InspectionPlan/openInspection.js
+++ client/src/components/InspectionPlan/openInspection.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './Inspection.css';
-import { columns, control_type } from './enumerated_inspection';
+import { columns, control_type, control_method } from './enumerated_inspection';
 import {
   getOpenInspectionPlans,
   getAllUsers,
@@ -19,6 +19,7 @@ import {
   handleApproveClick,
   handleRejectClick,
   handleCrossClick,
+  handleControlMethod,
 } from './inspection_utils';
 
 const Inspection = () => {
@@ -118,6 +119,25 @@ const Inspection = () => {
                 <td>{plan.order_number}</td>
                 <td>{plan.project_number}</td>
                 <td>{plan.quantity}</td>
+                <td>
+                  <select
+                    value={plan.control_method || ''}
+                    onChange={event =>
+                      handleControlMethod(
+                        event,
+                        plan.id,
+                        setInspectionPlans
+                      )
+                    }
+                  >
+                    <option value="">Select Control Method</option>
+                    {control_method.map((method, index) => (
+                      <option key={index} value={method}>
+                        {method}
+                      </option>
+                    ))}
+                  </select>
+                </td>
                 <td>
                   <select
                     value={plan.control_type || ''}
